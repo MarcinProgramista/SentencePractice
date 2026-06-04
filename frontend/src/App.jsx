d6980d43af11fef7f12f2778b22d3672e6c700da
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-06-04 18:06:29 +0200

    Display sentences in React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,30 @@
 import { useEffect } from "react";
 import { getSentences } from "./api/sentenceApi";
+import { useState } from "react";
 
 function App() {
+  const [sentences, setSentences] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await getSentences();
-      console.log(data);
+      setSentences(data);
     };
 
     fetchData();
   }, []);
 
-  return <h1>Language Learning</h1>;
+  return (
+    <>
+      <h1>Language Learning</h1>
+
+      {sentences.map((sentence) => (
+        <div key={sentence.id}>
+          {sentence.source_text} → {sentence.target_text}
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
